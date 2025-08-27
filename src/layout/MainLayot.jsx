c2e78c27ce43d7d74742e3c286adf47a764d072c
Văn Phan <haytv2222@gmail.com>
2025-08-27 09:58:01 +0700

    v

--- src/layout/MainLayot.jsx
+++ src/layout/MainLayot.jsx
@@ -1,9 +1,37 @@
+import { Bell, Compass, House, MessageCircle, Settings, SquarePlus } from "lucide-react";
 import { Outlet } from "react-router-dom";
 
 function MainLayot() {
   return (
-    <div>
-      <div>MainLayotHeared</div>
+    <div className="flex">
+      <div className=" flex flex-col items-center justify-between h-[100vh] w-[80px] py-8">
+       <div className=" flex flex-col gap-5"> 
+        <div className="h-[48px] w-[48px] flex items-center justify-center cursor-pointer">
+          <div className="modal_Logo h-[24px] w-[24px]">
+          </div>
+        </div>
+        <div className="h-[48px] w-[48px] flex items-center justify-center cursor-pointer rounded-[16px] hover:bg-[#18181612]">
+          <div><House /></div>
+        </div>
+        <div className="h-[48px] w-[48px] flex items-center justify-center cursor-pointer rounded-[16px] hover:bg-[#18181612]">
+          <div> <Compass /></div>
+        </div>
+        <div className="h-[48px] w-[48px] flex items-center justify-center cursor-pointer rounded-[16px] hover:bg-[#18181612]"> 
+          <div><SquarePlus /></div>
+        </div>
+        <div className="h-[48px] w-[48px] flex items-center justify-center cursor-pointer rounded-[16px] hover:bg-[#18181612]">
+          <div> <Bell /></div>
+        </div>
+        <div className="h-[48px] w-[48px] flex items-center justify-center cursor-pointer rounded-[16px] hover:bg-[#18181612]">
+          <div><MessageCircle /></div>
+        </div>
+        </div>
+
+
+        <div className="h-[48px] w-[48px] flex items-center justify-center cursor-pointer rounded-[16px] hover:bg-[#18181612]"> 
+        <Settings />
+        </div>
+      </div>
       <Outlet />
     </div>
   );
--- src/pages/auth/LoginRegister.page.jsx
+++ src/pages/auth/LoginRegister.page.jsx
@@ -16,17 +16,19 @@ function LoginRegisterPage() {
   };
 
   return (
-    <div className="h-[700px] bg-login-page w-screen flex flex-row items-center justify-center sm:justify-between px-6 lg:px-26 pb-[10px]">
+    <div className=" bg-login-page h-[calc(100vh-80px)] w-screen flex flex-row items-center justify-center sm:justify-between px-6 lg:px-26 pb-[10px]">
       <div className="hidden sm:flex items-center justify-start h-full flex-1 ">
         <h2 className="lg:text-[60px] md:text-[40px] sm:text-[25px] text-[70px] font-[600] text-white max-w-[475px]">
           Đăng nhập để nhận ý tưởng
         </h2>
       </div>
+      <div className="h=full flex items-center justify-between ">
       {isLoginModal ? (
         <LoginModal handleLoginWithGoogle={handleLoginWithGoogle} />
       ) : (
         <SignupModel handleLoginWithGoogle={handleLoginWithGoogle} />
       )}
+      </div>
     </div>
   );
 }
