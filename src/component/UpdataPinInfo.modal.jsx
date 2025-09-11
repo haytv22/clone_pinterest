ef2d24d67b83ab1d219cfcce469efdd512a63269
van phan <haytv2222@gmail.com>
2025-09-11 08:54:57 +0700

    vuive

--- src/component/UpdataPinInfo.modal.jsx
+++ src/component/UpdataPinInfo.modal.jsx
@@ -6,9 +6,34 @@ import toast from "react-hot-toast";
 
 function UpdataPinInfoModal({ isOpenModalPin, setIsOpenModalPin, pinTarget }) {
   console.log(pinTarget);
+  const descriptionRef =useRef()
+  const titleRef =useRef()
+  const outSideRef =useRef()
 
+
+  useEffect(() => {
+    const handleKeyDown = (e) => {
+      if (e.key === "Escape") {
+        setIsOpenModalPin(false);
+      }
+    };
+    document.addEventListener("keydown", handleKeyDown);
+    return () => document.removeEventListener("keydown", handleKeyDown);
+  }, []);
+
+  const handelClickUpdata = ()=>{
+    console.log(
+      descriptionRef.current.value,
+      titleRef.current.value
+      );
+  }
+  const clickOutSide  = (e)=>{
+   if (e.target == outSideRef.current) {
+    setIsOpenModalPin(false)
+   }
+  }
   return (
-    <div className="absolute inset-0 bg-gray-400/30 backdrop-blur-[10px] z-1000 flex items-center justify-center">
+    <div ref={outSideRef} onClick={clickOutSide} className="absolute inset-0 bg-gray-400/30 backdrop-blur-[10px] z-1000 flex items-center justify-center">
       <div className="w-[80%] bg-white rounded-2xl">
         <div className="h-[80px] w-full flex items-center justify-between px-5">
           <div
@@ -18,7 +43,7 @@ function UpdataPinInfoModal({ isOpenModalPin, setIsOpenModalPin, pinTarget }) {
             <X className="group-hover:block hidden size-[14px] font-bold" />
           </div>
           <div
-            onClick={""}
+            onClick={handelClickUpdata}
             className="p-3 px-5 btn-red rounded-2xl cursor-pointer text-white font-bold"
           >
             <p>lưu</p>
@@ -30,20 +55,25 @@ function UpdataPinInfoModal({ isOpenModalPin, setIsOpenModalPin, pinTarget }) {
             <div className="flex flex-col gap-3">
               <p className="font-bold">Ảnh</p>
               <img
-                className="object-contain max-h-[60vh]  rounded-2xl border-dashed border-4 boder-color"
+                className="object-contain max-h-[70vh]  rounded-2xl border-dashed border-4 boder-color"
                 src={pinTarget.image_url}
                 alt=""
               />
             </div>
           </div>
-          <div>
-            <div>
-              <label htmlFor="TieuDe">Tiêu đề</label>
-              <input type="text" defaultValue={""} id="TieuDe" />
+          <div className="flex flex-1 gap-5 flex-col">
+            <div className="w-full flex flex-col gap-3">
+              <label className="font-bold" htmlFor="TieuDe">Tiêu đề</label>
+              <input ref={titleRef} className="w-full bg-color px-5 rounded-2xl h-[60px] font-bold text-gray-800" type="text" defaultValue={pinTarget.title} id="TieuDe" />
             </div>
-            <div>
-              <label htmlFor="Mota">Mô tả</label>
-              <input type="text" defaultValue={""} id="MoTa" />
+            <div className="w-full flex flex-col gap-3">
+              <label className="font-bold" htmlFor="Mota">Mô tả</label>
+              <textarea
+                ref={descriptionRef}
+                className="w-full bg-color px-5 py-3 rounded-2xl h-[120px] outline-none"
+                id="moTa"
+                defaultValue={[pinTarget.description]}
+            ></textarea>
             </div>
           </div>
         </div>
